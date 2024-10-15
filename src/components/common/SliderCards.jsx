c8031554bf30f7e734c14cc8c7697b3347d2fff1
MajidKh230 <khanmajid230@gmail.com>
2024-10-16 00:47:55 +0500

    fetch products using api

--- src/components/common/SliderCards.jsx
+++ src/components/common/SliderCards.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Card from "./Card";
-import { products } from "../../Utils/Constant/productData";
+import axios from "axios";
+// import { products } from "../../Utils/Constant/productData";
 const SliderCards = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const data = await axios.get("https://fakestoreapi.com/products/");
+      console.log(data?.data);
+      const res = data?.data;
+      setProducts(res);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   const settings = {
     dots: false,
     infinite: true,
@@ -16,10 +34,8 @@ const SliderCards = () => {
   return (
     <div className="slider-container mt-10">
       <Slider {...settings}>
-        {products.map((data, index) => {
-          return (
-              <Card key={index} data={data} />
-          );
+        {products.reverse().map((data, index) => {
+          return <Card key={index} data={data} />;
         })}
       </Slider>
     </div>
